c0a7d3afaa16ec1b60fbedebf66a2f00fc618ee4
Yuval Dekel <dekel@yelp.com>
2019-07-26 14:13:31 -0700

    move works now

--- main.js
+++ main.js
@@ -10,24 +10,42 @@
  */
 
 class PlayerPosition {
-    constructor(x, y) {
-        this.hPos = x;
-        this.yPos = y;
+    constructor(h, v) {
+        this.hPos = v;
+        this.vPos = h;
     }
 }
 
 function play() {
-    let playerPos = new PlayerPosition(7, 3);
+    let playerPos = new PlayerPosition(3, -1);
     let board = [
-        [0, 0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
     ];
     printBoard(board, playerPos);
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+    playerPos = new PlayerPosition(4, -1)
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+    playerPos = new PlayerPosition(2, -1)
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+    playerPos = new PlayerPosition(9, -1)
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+    playerPos = new PlayerPosition(10, -1)
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+    playerPos = new PlayerPosition(11, -1)
+    board = move(playerPos,board);
+    printBoard(board, playerPos);
+
 }
 
 /**
@@ -35,7 +53,7 @@ function play() {
  * the board after the move occurs (if possible)
  */
 function move(playerPos, initialBoard) {
-    let isOnHorizontalRow = (playrPos.yPos == -1);
+    let isOnHorizontalRow = (playerPos.vPos == -1);
     if (isOnHorizontalRow) {
         let isOnTopRow = (Math.floor(playerPos.hPos / 7) == 0);
         let activeColumn = playerPos.hPos % 7;
@@ -72,13 +90,66 @@ function move(playerPos, initialBoard) {
             if (columnCleared) {
                 newColumn = [0,0,0,0,0,0,0];
             } else {
-                let slice = Array.prototype.splice.apply(newColumn.slice(0, stopPoint), [0,0,...(new Array(numToMove)).fill(0)]).slice(0, stopPoint)
+                let slice = newColumn.slice(0, stopPoint)
+                Array.prototype.splice.apply(slice, [0,0,...(new Array(stopPoint-1)).fill(0)])
+                slice = slice.slice(0, stopPoint)
                 Array.prototype.splice.apply(newColumn, [0, stopPoint, ...slice])
             }
             if (!isOnTopRow) { 
                 newColumn = newColumn.reverse();
             }
-            return initialBoard.map((row, i) => (row.slice(activeColumn, 1, newColumn[i])))
+            return initialBoard.map((row, i) => {
+                row.splice(activeColumn, 1, newColumn[i]);
+                return row;
+            });
+        }
+        return initialBoard;
+    } else {
+        let isOnLeftColumn = (Math.floor(playerPos.vPos / 7) == 0);
+        let activeRow = playerPos.vPos % 7;
+        if (initialBoard[activeRow][isOnLeftColumn?0:6]) {
+            let rowArray = initialBoard[activeRow]
+            if (!isOnLeftColumn) {
+                rowArray = rowArray.reverse();
+            }
+            if (!rowArray[0]) {
+                return initialBoard;
+            }
+            let numToMove = 1;
+            while (true) {
+                if (rowArray[numToMove]) {
+                    numToMove += 1;
+                } else {
+                    break;
+                }
+            }
+            let rowCleared = false;
+            let stopPoint = numToMove;
+            while (true) {
+                if (rowArray[stopPoint] == 0) {
+                    stopPoint += 1;
+                } else {
+                    break;
+                }
+                if (stopPoint < 0 || stopPoint > 6) {
+                    rowCleared = true;
+                    break;
+                }
+            }
+            let newRow = rowArray;
+            if (rowCleared) {
+                newRow = [0,0,0,0,0,0,0];
+            } else {
+                let slice = newRow.slice(0, stopPoint)
+                Array.prototype.splice.apply(slice, [0,0,...(new Array(stopPoint-1)).fill(0)])
+                slice = slice.slice(0, stopPoint)
+                Array.prototype.splice.apply(newRow, [0, stopPoint, ...slice])
+            }
+            if (!isOnLeftColumn) { 
+                newRow = newRow.reverse();
+            }
+            initialBoard.splice(activeRow, 1, newRow);
+            return initialBoard;
         }
         return initialBoard;
     }
@@ -89,23 +160,23 @@ function moveUser(playerPos, direction) {
 
     switch (direction) {
         case 'left':
-            if (playerPos.hPos >= 0 && (playerPos.yPos < 0 || playerPos.yPos > 6)) {
-                newPos = new PlayerPosition(playerPos.hPos - 1, playerPos.yPos);
+            if (playerPos.hPos >= 0 && (playerPos.vPos < 0 || playerPos.vPos > 6)) {
+                newPos = new PlayerPosition(playerPos.hPos - 1, playerPos.vPos);
             }
             break;
         case 'right':
-            if (playerPos.hPos <= 6 && (playerPos.yPos < 0 || playerPos.yPos > 6)) {
-                newPos = new PlayerPosition(playerPos.hPos + 1, playerPos.yPos);
+            if (playerPos.hPos <= 6 && (playerPos.vPos < 0 || playerPos.vPos > 6)) {
+                newPos = new PlayerPosition(playerPos.hPos + 1, playerPos.vPos);
             }
             break;
         case 'up':
-            if (playerPos.yPos >= 0 && (playerPos.hPos < 0 || playerPos.hPos > 6)) {
-                newPos = new PlayerPosition(playerPos.hPos, playerPos.yPos - 1);
+            if (playerPos.vPos >= 0 && (playerPos.hPos < 0 || playerPos.hPos > 6)) {
+                newPos = new PlayerPosition(playerPos.hPos, playerPos.vPos - 1);
             }
             break;
         case 'down':
-            if (playerPos.yPos <= 6 && (playerPos.hPos < 0 || playerPos.hPos > 6)) {
-                newPos = new PlayerPosition(playerPos.hPos, playerPos.yPos + 1);
+            if (playerPos.vPos <= 6 && (playerPos.hPos < 0 || playerPos.hPos > 6)) {
+                newPos = new PlayerPosition(playerPos.hPos, playerPos.vPos + 1);
             }
             break;
         default:
@@ -118,7 +189,7 @@ function moveUser(playerPos, direction) {
 function printBoard(board, playerPos) {
     // adjust player position to be in 9x9 grid
     let trueHPos = playerPos.hPos + 1;
-    let trueYPos = playerPos.yPos + 1;
+    let trueYPos = playerPos.vPos + 1;
 
     let buff = ''
 
